65931ef778fe26d59be9310d1ad6274421c96150
DFimp <ilia.fimp@gmail.com>
2024-03-10 14:21:28 +0300

    Realized connect to database

--- server/db.js
+++ server/db.js
@@ -2,4 +2,13 @@ const {Sequelize} = require("sequelize");
 
 
 // Экспортируем объект, который мы создаем из этого класса
-module.exports = new Sequelize()
+module.exports = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        dialect: "postgres",
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+    }
+)
