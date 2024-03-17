d546eadf6ca926de83616fbe76f6fa67a752d120
DFimp <ilia.fimp@gmail.com>
2024-03-17 12:49:46 +0300

    Realize page authorization and registration

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,10 +1,50 @@
 import React from 'react';
+import Container from 'react-bootstrap/esm/Container';
+import Form from 'react-bootstrap/esm/Form';
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Button from 'react-bootstrap/Button';
+import { NavLink, useLocation } from 'react-router-dom';
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from '../utils/consts';
 
 const Auth = () => {
+    const location = useLocation();
+    const isLogin = location.pathname === LOGIN_ROUTE;
+
     return (
-        <div>
-            AUTH
-        </div>
+        <Container
+            className="d-flex justify-content-center align-items-center"
+            style={{ height: window.innerHeight - 54 }}
+        >
+            <Card style={{ width: 600 }} className='p-5'>
+                <h2 className="m-auto">{isLogin ? "Авторизация" : "Регистрация"}</h2>
+                <Form className="d-flex flex-column">
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Введите ваш email..."
+                    />
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Введите пароль..."
+                    />
+                    {isLogin ?
+                        <div className="mt-3 align-self-center">
+                            Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйся!</NavLink>
+                        </div>
+                        :
+                        <div className="mt-3 align-self-center">
+                            Есть аккаунт? <NavLink to={LOGIN_ROUTE}>Войдите!</NavLink>
+                        </div>
+                    }
+                    <Button
+                        className="mt-3"
+                        variant="outline-success"
+                    >
+                        {isLogin ? "Войти" : "Регистрация"}
+                    </Button>{' '}
+                </Form>
+            </Card>
+        </Container>
     );
 };
 
