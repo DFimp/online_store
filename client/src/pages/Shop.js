cca8170721825ae6a4ab70d5358470819284dd5c
DFimp <ilia.fimp@gmail.com>
2024-03-20 00:00:47 +0300

    Added pagination and filtration

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -8,6 +8,7 @@ import DeviceList from '../components/DeviceList';
 import { observer } from 'mobx-react-lite';
 import { fetchBrands, fetchDevices, fetchTypes } from '../http/deviceAPI';
 import { Context } from '../index';
+import Pages from '../components/Pages';
 
 const Shop = observer(() => {
     const {device} = useContext(Context);
@@ -15,8 +16,18 @@ const Shop = observer(() => {
     useEffect(() => {
         fetchTypes().then(data => device.setTypes(data));
         fetchBrands().then(data => device.setBrands(data));
-        fetchDevices().then(data => device.setDevices(data.rows));
+        fetchDevices(null, null, 1, 3).then(data => {
+            device.setDevices(data.rows)
+            device.setTotalCount(data.count)        
+        });
     }, []);
+
+    useEffect(() => {
+        fetchDevices(device.selectedType.id, device.selectedBrand.id, device.page, 2).then(data => {
+            device.setDevices(data.rows)
+            device.setTotalCount(data.count)        
+        });
+    }, [device.page, device.selectedType, device.selectedBrand, ])
 
     return (
         <Container className="mt-2">
@@ -27,6 +38,7 @@ const Shop = observer(() => {
                 <Col>
                     <BrandBar />
                     <DeviceList/>
+                    <Pages/>
                 </Col>
             </Row>
         </Container>
